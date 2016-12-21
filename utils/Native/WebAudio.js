f006c10823567176e1bf616177792f2d666dca9e
Tessa Kelly <tessakkelly@gmail.com>
2016-12-21 16:15:05 -0700

    :bug: :ear: fixes clicking noise when stopping

--- utils/Native/WebAudio.js
+++ utils/Native/WebAudio.js
@@ -4,16 +4,20 @@ var audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
 var oscillator = function(frequency) {
     var oscillator = audioContext.createOscillator();
+    var gain = audioContext.createGain();
 
     oscillator.type = 'sine';
     oscillator.frequency.value = frequency; // value in hertz
-    oscillator.connect(audioContext.destination);
+    oscillator.connect(gain);
+    gain.connect(audioContext.destination);
     oscillator.start();
-    return oscillator;
+    return gain;
 }
 
 return {
     oscillator: oscillator,
-    stop: function(oscillator) { oscillator.stop(); },
+    stop: function(gainNode) {
+        gainNode.gain.setTargetAtTime(0, audioContext.currentTime, 0.015);
+    },
 };
 }();
