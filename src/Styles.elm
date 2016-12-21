6eeacffc6f227415255313d9d094272f9791853f
Tessa Kelly <tessakkelly@gmail.com>
2016-12-21 14:01:32 -0700

    Expose for and id from styels

--- src/Styles.elm
+++ src/Styles.elm
@@ -4,12 +4,15 @@ module Styles
         , css
         , class
         , id
+        , for
         , classList
         , snippets
         )
 
 import Css exposing (..)
 import Css.Namespace exposing (namespace)
+import Html
+import Html.Attributes
 import Html.CssHelpers exposing (Namespace, withNamespace)
 
 
@@ -24,13 +27,28 @@ snippets =
     ]
 
 
-{ class, classList, id } =
+{ class, classList } =
     currentNamespace
+
+
+id : String -> Html.Attribute msg
+id value =
+    Html.Attributes.id (namespace_ ++ value)
+
+
+for : String -> Html.Attribute msg
+for value =
+    Html.Attributes.for (namespace_ ++ value)
+
+
+namespace_ : String
+namespace_ =
+    "elm-piano-"
 
 
 currentNamespace : Html.CssHelpers.Namespace String a b c
 currentNamespace =
-    withNamespace "elm-piano-"
+    withNamespace namespace_
 
 
 css : String
