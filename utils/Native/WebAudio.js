4873411b0e39cafadc7bea8a63ecd9b1f5e51ba0
Tessa Kelly <tessakkelly@gmail.com>
2016-12-24 19:31:59 -0700

    fix the initial clicking noise

--- utils/Native/WebAudio.js
+++ utils/Native/WebAudio.js
@@ -5,11 +5,13 @@ var audioContext = new (window.AudioContext || window.webkitAudioContext)();
 var oscillator = function(frequency) {
     var oscillator = audioContext.createOscillator();
     var gain = audioContext.createGain();
+    gain.gain.value = 0;
 
     oscillator.type = 'sine';
     oscillator.frequency.value = frequency; // value in hertz
     oscillator.connect(gain);
     gain.connect(audioContext.destination);
+    gain.gain.setTargetAtTime(1, audioContext.currentTime, 0.015);
     oscillator.start();
     return gain;
 }
