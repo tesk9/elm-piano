cb770bd68e8dae697f15b756a1298d17655852dc
Tessa Kelly <tessakkelly@gmail.com>
2017-02-04 18:08:27 -0800

    Use custom wave if possible

--- utils/Native/WebAudio.js
+++ utils/Native/WebAudio.js
@@ -2,12 +2,30 @@ var _user$project$Native_WebAudio = function() {
 
 var audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
+var setWave = function(osc) {
+    var real = new Float32Array(2);
+    var imag = new Float32Array(2);
+
+    real[0] = 0;
+    imag[0] = 0;
+    real[1] = 1;
+    imag[1] = 0;
+
+    if (audioContext.createPeriodicWave) {
+        console.log("Creating periodic wave")
+        var wave = audioContext.createPeriodicWave(real, imag, {disableNormalization: true});
+        osc.setPeriodicWave(wave);
+    } else {
+        osc.type = 'sine';
+    }
+}
+
 var oscillator = function(frequency) {
     var oscillator = audioContext.createOscillator();
     var gain = audioContext.createGain();
     gain.gain.value = 0;
 
-    oscillator.type = 'sine';
+    setWave(oscillator)
     oscillator.frequency.value = frequency; // value in hertz
     oscillator.connect(gain);
     gain.connect(audioContext.destination);
